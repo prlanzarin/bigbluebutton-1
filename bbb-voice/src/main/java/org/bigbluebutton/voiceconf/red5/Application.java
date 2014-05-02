9a47d43fe0924a27a7525f8e12b54a84b03f2afe
Alexandre Kreismann <alexandrekreis@gmail.com>
2014-05-02 15:08:42 -0300

    The real app

--- bbb-voice/src/main/java/org/bigbluebutton/voiceconf/red5/Application.java
+++ bbb-voice/src/main/java/org/bigbluebutton/voiceconf/red5/Application.java
@@ -222,7 +222,6 @@ public class Application extends MultiThreadedApplicationAdapter {
 	private String getUsername() {
 		String username = (String) Red5.getConnectionLocal().getAttribute("USERNAME");
 		if ((username == null) || ("".equals(username))) username = "UNKNOWN-CALLER";
-		//return username;
-		return "Hello";
+		return username;
 	}
 }
