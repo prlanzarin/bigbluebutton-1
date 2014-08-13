e1cffeae1ce0317b31a30ed481268e26fee00879
Mario Gasparoni <mariogasparoni@gmail.com>
2014-08-13 16:08:53 -0300

    more adjusts

--- bbb-voice/src/main/java/org/bigbluebutton/voiceconf/red5/media/RtpStreamReceiver.java
+++ bbb-voice/src/main/java/org/bigbluebutton/voiceconf/red5/media/RtpStreamReceiver.java
@@ -1,10 +1,89 @@
+/**
+* BigBlueButton open source conferencing system - http://www.bigbluebutton.org/
+* 
+* Copyright (c) 2012 BigBlueButton Inc. and by respective authors (see below).
+*
+* This program is free software; you can redistribute it and/or modify it under the
+* terms of the GNU Lesser General Public License as published by the Free Software
+* Foundation; either version 3.0 of the License, or (at your option) any later
+* version.
+* 
+* BigBlueButton is distributed in the hope that it will be useful, but WITHOUT ANY
+* WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+* PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public License along
+* with BigBlueButton; if not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.bigbluebutton.voiceconf.red5.media;
+
+import java.io.IOException;
+import java.net.DatagramSocket;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import org.slf4j.Logger;
+import org.bigbluebutton.voiceconf.red5.media.net.RtpPacket;
+import org.bigbluebutton.voiceconf.red5.media.net.RtpSocket;
+import org.red5.logging.Red5LoggerFactory;
+
+public class RtpStreamReceiver {
+    protected static Logger log = Red5LoggerFactory.getLogger(RtpStreamReceiver.class, "sip");
+    
+    // Maximum blocking time, spent waiting for reading new bytes [milliseconds]     
+//    private static final int SO_TIMEOUT = 200;
+    private static int RTP_HEADER_SIZE = 12;
+    private RtpSocket rtpSocket = null;
+    private final Executor exec = Executors.newSingleThreadExecutor();
+	private Runnable rtpPacketReceiver;
+	private volatile boolean receivePackets = false;
+	private RtpStreamReceiverListener listener;
+    private final int payloadLength;
+    private int lastSequenceNumber = 0;
+
+    private long lastPacketTimestamp = 0;
+    private long lastTimestampDelta = 0;
+
+    private boolean firstPacket = true;
+    private boolean lastPacketDropped = false;
+    private int successivePacketDroppedCount = 0;
+    
+    private long lastPacketReceived = 0;
+    private long baseTimestamp = 0;
+    
+    public RtpStreamReceiver(DatagramSocket socket, int expectedPayloadLength) {
+    	this.payloadLength = expectedPayloadLength;
+        rtpSocket = new RtpSocket(socket);
+
+        initializeSocket();
+    }
+    
+    public void setRtpStreamReceiverListener(RtpStreamReceiverListener listener) {
+    	this.listener = listener;
+    }
+    
+    private void initializeSocket() {
+    }
+    
+    public void start() {
+    	receivePackets = true;
+    	rtpPacketReceiver = new Runnable() {
+    		public void run() {
+    			receiveRtpPackets();   			
+    		}
+    	};
+    	exec.execute(rtpPacketReceiver);
+    }
+    
+    public void stop() {
+    	receivePackets = false;
     }
     
     public void receiveRtpPackets() {    
         int packetReceivedCounter = 0;
         int internalBufferLength = payloadLength + RTP_HEADER_SIZE;
         byte[] internalBuffer = new byte[internalBufferLength];
-        RtpPacket rtpPacket = new RtpPacket(internalBuffer, internalBufferLength);
+        RtpPacket rtpPacket = new RtpPacket(internalBuffer, internalBufferLength);;
         
         while (receivePackets) {
         	try {       			
@@ -24,20 +103,25 @@
         		} else {
             		if (shouldHandlePacket(rtpPacket)) {
 
+                        //CHECK lastTimestampDelta
                         lastTimestampDelta = rtpPacket.getTimestamp() - lastPacketTimestamp;
                         
-                        //for debugging...
-                        if(packetReceivedCounter < 31)
-                            log.debug("rtpPacket.getLength() = " + rtpPacket.getLength()
-                                      + " | rtpPacket.getPayloadLength() = " + rtpPacket.getPayloadLength());
+                        
+                        
+                        //for debuging...
+                        if(packetReceivedCounter < 5000)
+                            log.debug(" rtpPacket.getTimestamp()= " + rtpPacket.getTimestamp());
                                 
+
+
 
                         lastPacketTimestamp = rtpPacket.getTimestamp();
 
             			lastSequenceNumber = rtpPacket.getSeqNum();
 
-                        byte[] newBuffer = internalBuffer.clone();
-            			processRtpPacket(newBuffer, RTP_HEADER_SIZE, rtpPacket.getPayloadLength());
+                        //check RTP_HEADER_SIZE
+                        byte freshData[] = internalBuffer.clone();
+            			processRtpPacket(freshData, RTP_HEADER_SIZE, rtpPacket.getPayloadLength());
             		
                     } else {
             			if (log.isDebugEnabled())
@@ -144,6 +228,7 @@
     }
 
     private void processRtpPacket(byte[] rtpMediaData, int offset, int len) {
+        //CHECK lastTimeStampDelta
 		if (listener != null) listener.onMediaDataReceived(rtpMediaData, offset, len, lastTimestampDelta);
 		else log.debug("No listener for incoming media packet");    	
     }
