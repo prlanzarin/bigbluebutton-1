2eeee16e7a20e9427ad39c965f3bf022e94dbe55
Alexandre <alexandrekreis@gmail.com>
2015-07-13 18:19:13 -0300

    Bug #1679 Testing NPE when we get the sip_user_agent

--- bigbluebutton-apps/src/main/java/org/bigbluebutton/webconference/voice/freeswitch/ESLEventListener.java
+++ bigbluebutton-apps/src/main/java/org/bigbluebutton/webconference/voice/freeswitch/ESLEventListener.java
@@ -273,7 +273,7 @@ public class ESLEventListener implements IEslEventListener {
          */
         String callerIdName = this.getCallerIdNameFromEvent(e);
         String sipUserAgent = this.getSipUserAgentFromEvent(e);
-        if (isUnknownCaller(callerIdName) && EquipmentTypes.isValidEquipment(sipUserAgent) && !sipUserAgent.equals(""))
+        if (isUnknownCaller(callerIdName) && sipUserAgent != null && !sipUserAgent.equals("") && EquipmentTypes.isValidEquipment(sipUserAgent))
             callerIdName = getCallerIdNameFromUserAgent(e); //we might truncate this string
         return callerIdName;
     }
