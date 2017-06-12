28b87eb32ca4a6538154955e3e3cf5aa273a903f
Pedro Beschorner Marin <pedrobmarin@gmail.com>
2017-06-12 13:57:38 +0000

    Removed FFmpeg processes from bbb-video

--- bbb-video/src/main/java/org/bigbluebutton/app/video/converter/VideoRotator.java
+++ bbb-video/src/main/java/org/bigbluebutton/app/video/converter/VideoRotator.java
@@ -1,8 +1,6 @@
 package org.bigbluebutton.app.video.converter;
 
 import org.apache.commons.lang3.StringUtils;
-import org.bigbluebutton.app.video.ffmpeg.FFmpegCommand;
-import org.bigbluebutton.app.video.ffmpeg.ProcessMonitor;
 import org.red5.logging.Red5LoggerFactory;
 import org.red5.server.api.IConnection;
 import org.red5.server.api.Red5;
@@ -22,10 +20,6 @@ public class VideoRotator {
 	public static final String ROTATE_UPSIDE_DOWN = "rotate_left/rotate_left";
 
 	private String streamName;
-//	private FFmpegCommand.ROTATE direction;
-
-	private FFmpegCommand ffmpeg;
-	private ProcessMonitor processMonitor;
 
 	/**
 	 * Create a new video rotator for the specified stream.
@@ -37,26 +31,6 @@ public class VideoRotator {
 	 */
 	public VideoRotator(String origin) {
 		this.streamName = getStreamName(origin);
-//		this.direction = getDirection(origin);
-
-//		log.debug("Setting up VideoRotator: StreamName={}, Direction={}",this.streamName,this.direction);
-		IConnection conn = Red5.getConnectionLocal();
-		String ip = conn.getHost();
-		String conf = conn.getScope().getName();
-		String inputLive = "rtmp://" + ip + "/video/" + conf + "/" + origin + " live=1";
-
-		String output = "rtmp://" + ip + "/video/" + conf + "/" + streamName;
-
-		ffmpeg = new FFmpegCommand();
-		ffmpeg.setFFmpegPath("/usr/local/bin/ffmpeg");
-		ffmpeg.setInput(inputLive);
-		ffmpeg.setFormat("flv");
-		ffmpeg.setOutput(output);
-		ffmpeg.setLoglevel("warning");
-//		ffmpeg.setRotation(direction);
-		ffmpeg.setAnalyzeDuration("10000"); // 10ms
-
-		start();
 	}
 	
 	/**
@@ -92,29 +66,6 @@ public class VideoRotator {
 				return ROTATE_UPSIDE_DOWN;
 			default:
 				return null;
-		}
-	}
-
-	/**
-	 * Start FFmpeg process to rotate and re-publish the stream.
-	 */
-	public void start() {
-//		log.debug("Spawn FFMpeg to rotate [{}] stream [{}]", direction.name(), streamName);
-		String[] command = ffmpeg.getFFmpegCommand(true);
-		if (processMonitor == null) {
-			processMonitor = new ProcessMonitor(command,"FFMPEG");
-		}
-		processMonitor.start();
-	}
-
-	/**
-	 * Stop FFmpeg process that is rotating and re-publishing the stream.
-	 */
-	public void stop() {
-//		log.debug("Stopping FFMpeg from rotate [{}] stream [{}]", direction.name(), streamName);
-		if(processMonitor != null) {
-			processMonitor.destroy();
-			processMonitor = null;
 		}
 	}
 
