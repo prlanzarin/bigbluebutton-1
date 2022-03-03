a741efce06023217f29769047af108260555d111
prlanzarin <4529051+prlanzarin@users.noreply.github.com>
2022-03-03 11:53:31 -0300

    build: update KMS docker build files
    Makes it run on 18.04, update variables, update healthchecker and use the 2.4 BBB source repo by default
    
    TODO: KMS should not run as root

--- labs/docker/kurento/docker-entrypoint.sh
+++ labs/docker/kurento/docker-entrypoint.sh
@@ -4,17 +4,54 @@ apt-get install -y openh264-gst-plugins-bad-1.5
 
 rm -f /etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
 # Generate WebRtcEndpoint configuration
+if [ "$STUN_IP" != "" ]; then
 echo "stunServerAddress=$STUN_IP" >> /etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
+fi
+
+if [ "$STUN_PORT" != "" ]; then
 echo "stunServerPort=$STUN_PORT" >> /etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
+fi
 
 if [ "$TURN_URL" != "" ]; then
   echo "turnURL=$TURN_URL" >> /etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
+fi
+
+if [ "$NETWORK_INTERFACES" != "" ]; then
+  echo "networkInterfaces=$NETWORK_INTERFACES" >> /etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
+fi
+
+if [ "$IP_IGNORE_LIST" != "" ]; then
+  echo "ipIgnoreList=$IP_IGNORE_LIST" >> /etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
+fi
+
+if [ "$EXTERNAL_IPV4" != "" ]; then
+  echo "externalIPv4=$EXTERNAL_IPV4" >> /etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
+fi
+
+if [ "$EXTERNAL_IPV6" != "" ]; then
+  echo "externalIPv6=$EXTERNAL_IPV6" >> /etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
+fi
+
+if [ "$NICE_AGENT_ICE_TCP" != "" ]; then
+  echo "iceTcp=$NICE_AGENT_ICE_TCP" >> /etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
+fi
+
+if [ "$PEM_CERTIFICATE_RSA" != "" ]; then
+  echo "pemCertificateRSA=$PEM_CERTIFICATE_RSA" >> /etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
+fi
+
+if [ "$PEM_CERTIFICATE_ECDSA" != "" ]; then
+  echo "pemCertificateECDSA=$PEM_CERTIFICATE_ECDSA" >> /etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
 fi
 
 rm -f /etc/kurento/modules/kurento/BaseRtpEndpoint.conf.ini
 # Generate BaseRtpEndpoint configuration
 echo "minPort=$RTP_MIN_PORT" >> /etc/kurento/modules/kurento/BaseRtpEndpoint.conf.ini
 echo "maxPort=$RTP_MAX_PORT" >> /etc/kurento/modules/kurento/BaseRtpEndpoint.conf.ini
+
+rm -f /etc/kurento/modules/kurento/RecorderEndpoint.conf.ini
+# Generate RecorderEndpoint configuration
+echo "gapsFix=$RECORDER_GAPS_FIX" >> /etc/kurento/modules/kurento/RecorderEndpoint.conf.ini
 
 CONFIG=$(cat /etc/kurento/kurento.conf.json | sed '/^[ ]*\/\//d' | jq ".mediaServer.net.websocket.port = $PORT")
 echo $CONFIG > /etc/kurento/kurento.conf.json
--- labs/docker/kurento/healthchecker.sh
+++ labs/docker/kurento/healthchecker.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-[[ "$(curl -w '%{http_code}' -N -H "Connection: Upgrade" -H "Upgrade: websocket" -H "Host: 127.0.0.1:$PORT" -H "Origin: 127.0.0.1" http://127.0.0.1:$PORT/kurento)" == 500 ]] && exit 0 || exit 1
+set -o errexit -o nounset
+
+RC="$(curl \
+  --silent \
+  --no-buffer \
+  --connect-timeout 4 \
+  --max-time 4 \
+  --write-out '%{http_code}' \
+  --header "Connection: Upgrade" \
+  --header "Upgrade: websocket" \
+  --header "Host: 127.0.0.1:$PORT" \
+  --header "Origin: 127.0.0.1" \
+  http://127.0.0.1:$PORT/kurento)"
+
+if [[ "$RC" == "500" ]]; then
+  exit 0
+else
+  exit 1
+fi
