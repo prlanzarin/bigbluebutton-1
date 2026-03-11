antobinary/bigbluebutton

refactor: typed voice user metadata for audio state hooks (name, speechLocale, color)

bigbluebutton-html5/imports/ui/components/nav-bar/nav-bar-graphql/talking-indicator/component.tsx
@@ -12,6 +12,7 @@ import useTalkingUsers from '/imports/ui/core/hooks/useTalkingUsers';
 import { partition } from '/imports/utils/array-utils';
 import logger from '/imports/startup/client/logger';
 import connectionStatus from '/imports/ui/core/graphql/singletons/connectionStatus';
+import { VoiceUserMetadata } from '/imports/ui/core/hooks/types';
 
 const TALKING_INDICATORS_MAX = 8;
 
@@ -46,7 +47,7 @@ interface TalkingIndicatorProps {
   talkingUsers: {
     talking: boolean;
     muted: boolean;
-    user: { color: string; speechLocale?: string; name: string };
+    user: VoiceUserMetadata;
     userId: string;
   }[];
   isBreakout: boolean;
@@ -141,7 +142,7 @@ const TalkingIndicator: React.FC<TalkingIndicatorProps> = ({
           icon={icon}
           size="lg"
           style={
-            isModerator
+            (isModerator && color)
               ? {
                 backgroundColor: color,
                 border: `solid 2px ${color}`,

prlanzarin/bigbluebutton-1

bigbluebutton-html5/imports/ui/components/nav-bar/nav-bar-graphql/talking-indicator/component.tsx
@@ -12,6 +12,7 @@ import useTalkingUsers from '/imports/ui/core/hooks/useTalkingUsers';
 import { partition } from '/imports/utils/array-utils';
 import logger from '/imports/startup/client/logger';
 import connectionStatus from '/imports/ui/core/graphql/singletons/connectionStatus';
+import { VoiceUserMetadata } from '/imports/ui/core/hooks/types';
 
 const TALKING_INDICATORS_MAX = 8;
 
@@ -46,7 +47,7 @@ interface TalkingIndicatorProps {
   talkingUsers: {
     talking: boolean;
     muted: boolean;
-    user: { color: string; speechLocale?: string; name: string };
+    user: VoiceUserMetadata;
     userId: string;
   }[];
   isBreakout: boolean;
@@ -143,7 +144,7 @@ const TalkingIndicator: React.FC<TalkingIndicatorProps> = ({
           icon={icon}
           size="lg"
           style={
-            isMuteActionAvailable
+            (isMuteActionAvailable && color)
               ? {
                 backgroundColor: color,
                 border: `solid 2px ${color}`,